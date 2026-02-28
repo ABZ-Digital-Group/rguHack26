a9f7dc7273af96791a152f1d8003659cfd1c6e69
Allypmc <a.mcculloch3@rgu.ac.uk>
2026-02-28 16:05:41 +0000

    compareData script

--- public/scripts/calculateValues.js
+++ public/scripts/calculateValues.js
@@ -6,15 +6,15 @@ CO2 values = kg per km
 Calories =kcal per km
 */
 
-const transportStats = {
+export const transportStats = {
     car: {speed: 60, co2: 0.21, calories: 0},
     bus: {speed: 40, co2: 0.10, calories: 0},
     train: {speed: 80, co2: 0.05, calories: 0},
     bike: {speed: 15, co2: 0, calories: 40},
     walk: {speed: 5, co2: 0, calories: 60},
-}
+};
 
-function calculateJourneyStats(journeyInput){
+export function calculateJourneyStats(journeyInput){
     const stats = transportStats[journeyInput.method];
 
     if(!stats){
---
+++ public/scripts/compareData.js
@@ -0,0 +1,26 @@
+import transportLogs from "./journeyInput.js";
+import { calculateJourneyStats } from "./calculateValues.js";
+
+function compareWithCar(journeyInput){
+    const userStats = calculateStats(journeyInput.method, journeyInput.distanceKM);
+    const carStats = calculateStats("car", journeyInput.distanceKM);
+
+    if(!userStats || !carStats){
+        console.log(`Unknown method: ${journeyInput.distnaceKM}`);
+        return null;
+    }
+
+    return{
+        method: journeyInput.method,
+        distnaceKM: journeyInput.distanceKM,
+
+        timeDifferenceHours: (userStats.time - carStats.time).toFixed(2),
+        co2SavedKG: (carStats.co2 - userStats.co2).toFixed(2),
+        extraCaloriesBurned: (userStats.calories - carStats.calories).toFixed(0)
+    };
+}
+
+const comparisonResults = transportLogs.map(compareWithCar);
+
+console.log("Comparison vs Driving Car:");
+console.log(comparisonResults);
